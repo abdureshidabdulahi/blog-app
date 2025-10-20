3c9119250a14724600bcfa7fb0596917b9f07511
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-10-20 15:56:07 +0300

    update

--- blog/src/home.jsx
+++ blog/src/home.jsx
@@ -7,7 +7,7 @@ export default function Home(){
             <h1>Share stories that matter.</h1>
             <p>welcom to <span>Blog</span> - amodern platform to write,read,and connect throught a powerfull content.</p>
             <div className='twinButtons'><button className='one'>Browse Blogs</button><button className='two'>Get Started</button></div>
-            <div className='search'><input type='text'/></div>
+            <div className='search'><input type='text' placeholder='search ....'/></div>
         </div>
     )
 }
