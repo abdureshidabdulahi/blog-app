728a4d0239ac24b5f3fad01ec00c796a382264d1
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-10-20 14:38:06 +0300

    updadte

--- blog/src/header.js
+++ blog/src/header.js
@@ -3,9 +3,15 @@ import './App.css'
 export default function Header(){
     return(
         <div className="header-container">
-            <div><img src={image1}  height={100}/></div>
-            <div></div>
-            <div></div>
+            <div><img src={image1}  height={70}/></div>
+           
+         <div className='info'>
+         <span>Home</span>
+            <span>Blogs</span>
+           <span>About</span>
+         </div>
+            <div><button className='signin'>Signin</button></div>
+           
         </div>
     )
 }
