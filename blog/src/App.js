8cb9ee7cb66291495423001fb0d31a937bbdd468
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-10-20 15:36:43 +0300

    update

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from './header';
+import Home from './home';
 function App() {
   return (
     <div className="App">
        <Header/>
+       <Home />
     </div>
   );
 }
--- blog/src/home.jsx
+++ blog/src/home.jsx
@@ -1,3 +1,13 @@
+
+import './App.css'
 export default function Home(){
-    return
+    return(
+        <div className="home-container">
+            <h1>Elevate your voice.</h1>
+            <h1>Share stories that matter.</h1>
+            <p>welcom to <span>Blog</span> - amodern platform to write,read,and connect throught a powerfull content.</p>
+            <div className='twinButtons'><button className='one'>Browse Blogs</button><button className='two'>Get Started</button></div>
+            <div className='search'><input type='text'/></div>
+        </div>
+    )
 }
