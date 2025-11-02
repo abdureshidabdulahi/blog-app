3a153d8ef46b65f9a5a2204f0ee0c5003418eef4
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-11-02 21:03:43 +0300

    update

--- blog/src/login.js
+++ blog/src/login.js
@@ -1,11 +1,13 @@
+import './App.css'
+
 export default function Login(){
     return(
         <div className="login-container">
             <div className="login">
             <h3>Login Into Your Account</h3>
-            <input type="text" placeholder="Your Email" />
-            <input type="text" placeholder="Your password" />
-            <button>SignIn</button>
+            <input type="text" placeholder="Your Email" className='login-input'/>
+            <input type="text" placeholder="Your password" className='login-input'/>
+            <button className='login-button'>SignIn</button>
             <p>Don't Have Account? <span>Register Here</span></p>
             </div>
         </div>
