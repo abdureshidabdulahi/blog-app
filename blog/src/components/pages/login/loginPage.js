32b9ffdc82e61eb4d601e5bcfe23fec78d187df9
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-03-14 12:59:50 +0300

    update

--- blog/src/components/pages/login/loginPage.js
+++ blog/src/components/pages/login/loginPage.js
@@ -1,11 +1,28 @@
+import { useState } from 'react'
 import './login.css'
 
 
 const LoginPage = ()=>{
+    const [signin,setSignin] =useState('Login')
     return(
          <div className="login-page">
-            <h1>Login</h1>
+            <h1>{signin}</h1>
             <div className="login">
+
+                {
+                    signin === 'Signin' ?
+                    <>
+                     <div>
+                    <p>Enter fristName</p>
+                    <input type='text' placeholder='Enter fristName'/>
+                </div>
+                <div>
+                    <p>Enter lastName</p>
+                    <input type='text' placeholder='Enter lastName'/>
+                </div>
+                    </>:<></>
+                }
+
             <div>
                 <p>Email</p>
                 <input type="email" placeholder="Enter your Email" />
@@ -14,12 +31,19 @@ const LoginPage = ()=>{
                 <p>Password</p>
                 <input type="password" placeholder="Enter your Password" />
             </div>
+           {
+            signin === 'Signin' ? <div>
+                <p>Confirm Password</p> 
+                <input type='password' placeholder='Confirm Password' />
+            </div> :<></>
+           }
            <div className="checkbox">
              <input type="checkbox" /> 
             <p>Accept to continue this.</p>
            </div>
-           <button>login</button>
-           <p>Don`t have account? <span>Signin here</span></p>
+           <button>{signin}</button>
+           {signin ==='Login'?<p>Don`t have account? <span onClick={()=>setSignin('Signin')}>Signin here</span></p>
+           :<p>Already have account? <span onClick={()=>setSignin('Login')}>Login here</span></p>}
             </div>
          </div>
 
