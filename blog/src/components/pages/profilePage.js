7b30764511183f273bd7b21f2cd833fd4de52111
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-03-16 23:35:15 +0300

    update

---
+++ blog/src/components/pages/profilePage.js
@@ -0,0 +1,16 @@
+
+
+const ProfilePage =()=>{
+    return(
+        <div className="profile-page">
+            <div className="profile-blogs">
+            
+            </div>
+            <div className="profile-profile"> 
+
+            </div>
+        </div>
+    )
+}
+
+export default ProfilePage
