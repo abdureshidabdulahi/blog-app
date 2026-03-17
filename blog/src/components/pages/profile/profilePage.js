01635f84f9bde35112d20d8e2352b825505269c4
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-03-17 11:32:12 +0300

    update

--- blog/src/components/pages/profile/profilePage.js
+++ blog/src/components/pages/profile/profilePage.js
@@ -1,4 +1,6 @@
- import './profile.css'
+ import { data } from 'react-router-dom'
+import './profile.css'
+
 
 const ProfilePage =()=>{
     return(
@@ -6,16 +8,16 @@ const ProfilePage =()=>{
             <div className="profile-blogs">
                 <h2>My Blogs</h2>
                 <div>
-
+                        hi its me
                 </div>
             
             </div>
             <div className="profile-profile"> 
-                <img src="" alt="profile-phot" />
+                <img src='/assets/erick-chevez-WSD3UnbB6ZI-unsplash.jpg' alt="profile-phot" width={150} height={150}/>
                 <p>@user</p>
                 <p>0 blogs - 0 reads</p>
                 <button>Edit Profile</button>
-                <p>no bio here</p>
+                <p className='bio'>no bio here</p>
                 <p>joined at december 9</p>
 
 
