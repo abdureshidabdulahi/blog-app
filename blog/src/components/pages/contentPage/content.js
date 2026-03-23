2069e297146a2cecfa288c3d3cc604d89074dab4
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-03-23 23:11:25 +0300

    update

--- blog/src/components/pages/contentPage/content.js
+++ blog/src/components/pages/contentPage/content.js
@@ -2,16 +2,36 @@
  import './content.css'
  import 'react-quill/dist/quill.snow.css'
  import axios from 'axios' 
+import { useState } from "react"
 
  const BlogForm = ()=>{
+    const [image,setImage] =useState(null)
+    const [previewUrl,setPreviewUrl] = useState(null)
+
+    const trigerFileInput = ()=>{
+        document.getElementById('input-file').click()
+    }
+
+    const handlechange =(event)=>{
+    const file = event.target.files[0]
+        if(!file) return
+        const previewImage = URL.createObjectURL(file)
+        setImage(file)
+        setPreviewUrl(previewImage)
+    }
     return(
         <div className="container">
            <div className="headers">
-           <h2>Create Content</h2>
+           <h1>Create Content</h1>
+           {
+            image?<img src={previewUrl} alt="previewphoto" onClick={trigerFileInput}/>:<img src="/assets/upload_area.png" alt="upload" onClick={trigerFileInput}/>
+           }
+           <input type="file"  id="input-file" onChange={handlechange}/>
             <input type="text" placeholder="please enter title"/>
             <input type="text" placeholder="please enter author"/>
             <input type="text" placeholder="please enter category"/>
            </div>
+           <h2>Enter Your Description Below</h2>
            <div className="content" >
              <form>
                 <ReactQuill placeholder="please write your descriptions here"/>
