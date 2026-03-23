e335407243a6c6837c13d328e35f865501e78d2e
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-03-24 00:17:53 +0300

    update

--- blog/src/components/pages/contentPage/content.js
+++ blog/src/components/pages/contentPage/content.js
@@ -2,11 +2,17 @@
  import './content.css'
  import 'react-quill/dist/quill.snow.css'
  import axios from 'axios' 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
  const BlogForm = ()=>{
     const [image,setImage] =useState(null)
     const [previewUrl,setPreviewUrl] = useState(null)
+    const [content,setContent] = useState({
+        title:'',
+        author:'',
+        category:'',
+        contents:''
+    })
 
     const trigerFileInput = ()=>{
         document.getElementById('input-file').click()
@@ -19,6 +25,21 @@ import { useState } from "react"
         setImage(file)
         setPreviewUrl(previewImage)
     }
+    const handleInput = (event)=>{
+        const value = event.target.value
+        const name = event.target.name
+        setContent({...content,[name]:value})
+    }
+    const handleQuill = (value)=>{
+        setContent({
+            ...content,
+            contents:value
+        })
+    }
+
+    useEffect(()=>{
+        console.log(content)
+    },[])
     return(
         <div className="container">
            <div className="headers">
@@ -27,14 +48,14 @@ import { useState } from "react"
             image?<img src={previewUrl} alt="previewphoto" onClick={trigerFileInput}/>:<img src="/assets/upload_area.png" alt="upload" onClick={trigerFileInput}/>
            }
            <input type="file"  id="input-file" onChange={handlechange}/>
-            <input type="text" placeholder="please enter title"/>
-            <input type="text" placeholder="please enter author"/>
-            <input type="text" placeholder="please enter category"/>
+            <input type="text" placeholder="please enter title" value={content.title} name="title" onChange={handleInput}/>
+            <input type="text" placeholder="please enter author" value={content.author} name="author" onChange={handleInput}/>
+            <input type="text" placeholder="please enter category" value={content.category} name="category" onChange={handleInput}/>
            </div>
            <h2>Enter Your Description Below</h2>
            <div className="content" >
              <form>
-                <ReactQuill placeholder="please write your descriptions here"/>
+                <ReactQuill placeholder="please write your descriptions here" value={content.contents}  onChange={handleQuill}/>
              </form>
            </div>
 
