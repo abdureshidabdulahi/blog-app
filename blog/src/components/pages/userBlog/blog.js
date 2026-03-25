2238ffcd7c22a92e39a3d8cfe4ff7c40bd0ae472
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-03-25 10:50:20 +0300

    update

--- blog/src/components/pages/userBlog/blog.js
+++ blog/src/components/pages/userBlog/blog.js
@@ -1,25 +1,44 @@
 import { useContext } from "react";
 import { storeContext } from "../../context/storeContext";
-import './userBlog.css'
-import dompurify from 'dompurify'
+import "./userBlog.css";
+import DOMPurify from "dompurify";
 
- 
 const Blog = () => {
-  const {myBlogs} = useContext(storeContext)
-   return(
+  const { myBlogs } = useContext(storeContext);
+
+  //Function to extract plain text and truncate it
+  const getShortText = (html, limit = 120) => {
+    const temp = document.createElement("div");
+    temp.innerHTML = html;
+
+    const text = temp.textContent || temp.innerText || "";
+
+    return text.length > limit
+      ? text.substring(0, limit) + "..."
+      : text;
+  };
+
+  return (
     <div className="container">
-    {
-      myBlogs.map((item)=>(
-        <div className="each-content-container">
-          {<img src={ `http://localhost:5137/images/${item.image}`} alt="header" />}
+      {myBlogs.map((item, index) => (
+        <div className="each-content-container" key={index}>
+          <img
+            src={`http://localhost:5137/images/${item.image}`}
+            alt="header"
+          />
+
           <h1>{item.title}</h1>
-         <div dangerouslySetInnerHTML={{__html:dompurify.sanitize(item.description)}} className="quill"/>
-         <hr />
+
+          {/*Show shortened text instead of full HTML */}
+          <p className="quill">
+            {getShortText(item.description, 120)}
+          </p>
+
+          <hr /> 
         </div>
-      ))
-    }
+      ))}
     </div>
-   )
+  );
 };
 
 export default Blog;
