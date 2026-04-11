98d66f97ed8c513f5331423030a3458bda27261b
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-04-11 10:46:11 +0300

    update

--- blog-backend/components/controlers/likeComent.js
+++ blog-backend/components/controlers/likeComent.js
@@ -7,14 +7,26 @@ export const handleLikes = async (req, res) => {
         if (!post) {
             return res.status(404).json({ message: "Post not found" });
         }
-        if(pas)
-
-        // Push the new like to the array
-        post.likes.push({ userId: req.userId });
-
+        const likes = post.likes
+        const result = likes.some((liked)=> liked.userId === req.userId)
+        console.log('this is the result',result)
+        // console.log(likes)  
+        if(result === false){
+             // Push the new like to the array
+            post.likes.push({ userId: req.userId });
+             
+            
+           
+        } else {
+            const filteredLikes = likes.filter((liked) => liked.userId !== req.userId)
+            post.likes = filteredLikes
+        }
         // Save the updated post to the database
         await post.save();
-        console.log(post)
+        
+
+       
+        // console.log(post)
         // Respond with success and optionally the updated likes
         res.status(200).json({
             message: "Like added successfully",
