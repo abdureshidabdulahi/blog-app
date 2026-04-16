1c2f5457e979cf46807dce3f5509503fbe7a44fe
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2026-04-16 15:29:07 +0300

    update

--- blog/src/components/pages/settings/settings.js
+++ blog/src/components/pages/settings/settings.js
@@ -1,12 +1,19 @@
  
 import './settings.css'
+import { useRef } from 'react'
 import ChangePass from './changePass'
 import { useEffect, useState } from 'react'
 const SettingsPage = ()=>{ 
     const [value,setValue] =useState(true)
+    const [profile,setProfile] = useState(null)
     const [data,setData] = useState({
         username:'',
-        gmail:''
+        gmail:'',
+        bio:'',
+        youtube:'',
+        facebook:'',
+        telegram:'',
+        whatsApp:'',
 
     })
     const handleChange = (event)=>{
@@ -16,6 +23,17 @@ const SettingsPage = ()=>{
         
 
     }
+    const onchangeFile = (event)=>{
+    const file = event.target.files[0]
+    setProfile(file)
+    console.log('this is the files',file)
+    if(!file){
+        console.log('there is no file')
+    }
+    }
+const handleprofileClick =()=>{  
+    document.getElementById('input-field').click()
+}
     const handlePassword = ()=>{
         setValue(false)
     }
@@ -40,8 +58,16 @@ const SettingsPage = ()=>{
               <div className="main"> 
                <div className='main-profile'>
                  <h2>Edit Profile</h2>
-                 <img src='/assets/d12dfdbd6b7501faf694ac42775f19451aee8805-324x328.webp' alt="editphoto" width={150} height={150}/>
-                 <button>Upload</button>
+                 
+               {
+                profile === null &&(
+                     <img src='/assets/upload_area.png' alt="editphoto" className='profile-photo' width={150} height={150} 
+                 onClick={handleprofileClick}/>
+                )
+               }
+                 
+                 <input type='file' className='input-file' id='input-field' ref={inputRef} onChange={onchangeFile}/>
+                 
                </div>
                <div className='main-profile2'> 
                 <div className="header-input"> 
@@ -50,15 +76,16 @@ const SettingsPage = ()=>{
                 </div>
                 <input onChange={handleChange} name='username' type="text" placeholder='Enter Username' className='input'/>
                 <p>Username will be used to search the user and will be visible to all users.</p>
-                <textarea onChange={handleChange} placeholder="Bio" rows={5} cols={10}/>
+                <textarea name='bio' onChange={handleChange} placeholder="Write your Bio" rows={5} cols={10}/>
                 
                 <h2>Add Your Social Media Account</h2>
                 <div className='social-media'>
-                    <input type="text" placeholder="https:// youtub"/>
-                    <input type="text" placeholder="https:// telegram"/>
-                    <input type="text" placeholder="https:// facebook"/>
-                    <input type="text" placeholder="https://whatsApp"/>
+                    <input type="text" onChange={handleChange} name='youtube' placeholder="https:// youtube"/>
+                    <input type="text" onChange={handleChange} name='telegram' placeholder="https:// telegram"/>
+                    <input type="text" onChange={handleChange} name='facebook' placeholder="https:// facebook"/>
+                    <input type="text" onChange={handleChange} name='whatsApp' placeholder="https:// whatsApp"/>
                 </div>
+                <button>Upload Changes</button>
                </div>
             </div>
            </form>:<ChangePass/>
